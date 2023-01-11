de82cf813b75e579422989b15b94aa33d51ec6a0
Jose Guerra <jose1894@gmail.com>
2023-01-11 13:13:20 -0400

    Primero commit del proyecto

--- src/main.js
+++ src/main.js
@@ -1,7 +1,24 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import './style.css'
 
-createApp(App).use(store).use(router).mount('#app')
+import './css/bootstrap/css/bootstrap.min.css'
+import './css/font-awesome/css/font-awesome.min.css'
+import './js/datetimepicker/bootstrap-datetimepicker.min.css'
+import './js/owl-carousel/owl.carousel.css'
+import './css/themecss/lib.css'
+import './js/jquery-ui/jquery-ui.min.css'
+import './js/minicolors/miniColors.css'
+import './css/themecss/so_searchpro.css'
+import './css/themecss/so_megamenu.css'
+import './css/themecss/so-categories.css'
+import './css/themecss/so-listing-tabs.css'
+import './css/themecss/so-newletter-popup.css'
+import './css/footer/footer1.css'
+import './css/header/header1.css'
+import './css/theme.css'
+import './css/responsive.css'
+
+
+import App from './App.vue'
+
+createApp(App).mount('#app')
